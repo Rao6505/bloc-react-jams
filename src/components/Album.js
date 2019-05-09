d47e558121987abc7abde80d231f51a5b8489157
Derek <solerpower1@hotmail.com>
2019-05-08 22:44:12 -0700

    Checkpoint 7 AudioP: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,7 +12,8 @@ class Album extends Component {
 		this.state = {
 			album: album,
 			currentSong: album.songs[0],
-			isPlaying: false
+			isPlaying: false,
+			isHovered: false,
 		};
 
 		this.audioElement = document.createElement('audio');
@@ -65,11 +66,20 @@ class Album extends Component {
 					</colgroup>
 					<tbody>
 						{this.state.album.songs.map((song, index) =>
-							<tr className="song"
+							<tr
+								className="song"
 								key={index}
 								onClick={() => this.handleSongClick(song)}
 								onMouseEnter={() => this.setState({ isHovered: index + 1 })}
 								onMouseLeave={() => this.setState({ isHovered: false })}>
+								<td>
+									<button id="song-action-buttons" >
+										{this.state.currentSong.title === song.title ?
+											(<span className={this.state.isPlaying ? "ion-pause" : "ion-play"} />) :
+											this.state.isHovered === index + 1 ? (<span className="ion-play" />) :
+												(<span className="song-number">{index + 1}</span>)}
+									</button>
+								</td>
 								<td> {song.number} {index + 1} </td>
 								<td> {song.title} </td>
 								<td> {song.duration} </td>
